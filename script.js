34e926ff59a6c0e31e0669d2a152f751d61bb7eb
adrianmmaulana <141643671+adrianmaulana568@users.noreply.github.com>
2024-11-22 10:54:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const navbarNav = document.querySelector('.navbar-nav');
+const hamburger = document.querySelector('#hamburger-menu');
+
+
+hamburger.onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
